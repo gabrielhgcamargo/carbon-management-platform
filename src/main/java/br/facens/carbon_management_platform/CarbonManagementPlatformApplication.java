b93a1f5ebd5de14db3bbbb357c1b3a8fa28950f1
gabrielhgcamargo <gacamargos4@gmail.com>
2024-06-08 14:03:54 -0300

    first commit

---
+++ src/main/java/br/facens/carbon_management_platform/CarbonManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package br.facens.carbon_management_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonManagementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbonManagementPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/facens/carbon_management_platform/CarbonManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package br.facens.carbon_management_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
